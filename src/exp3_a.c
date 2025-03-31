3e641cb741f38adc65aed33cfaebf3120f0ca7d3
Ajas Daison <ajas.daison@gmail.com>
2025-03-31 17:54:37 +0530

    Update

---
+++ src/exp3_a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  int pid;
+  // Create a child process
+  pid = fork();
+
+  if (pid < 0) {
+    // Check if fork failed
+    printf("fork failed\n");
+    // if error occurs
+    exit(1);
+  } else if (pid == 0) {
+    // Child process
+    execlp("/bin/date", "date",
+           NULL); // Replace child process with 'date' command
+    exit(0);
+  } else { // Parent process
+    printf("Parent Process id is - %d\n Child Process id is - %d\n", getpid(),
+           pid);
+    wait(NULL); // Wait for child process to complete
+    exit(0);
+  }
+
+  return 0;
+}
