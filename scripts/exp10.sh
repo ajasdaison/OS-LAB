58e144e616e4d1b25065636823cffdfbdd6bc3d5
Ajas Daison <ajas.daison@gmail.com>
2025-04-06 15:36:46 +0530

    added exp10_b.c

--- scripts/exp10.sh
+++ scripts/exp10.sh
@@ -12,3 +12,18 @@ echo "Running exp10_a.c ...."
 4
 4
 EOF
+
+echo "________________________________"
+
+# --- SCAN DISK SHED (exp10_a.c) ---
+echo "Compiling the Scan Disk (exp10_b) ..."
+gcc src/exp10_b.c -o src/exp10_b
+
+echo "Running exp10_b.c ...."
+./src/exp10_b <<EOF
+3
+4
+3
+4
+4
+EOF
